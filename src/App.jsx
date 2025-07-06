5e36eb37c1f521f8f793542ebc1c9fd9d9517ac4
Anchal <155175388+Anchal-T@users.noreply.github.com>
2025-07-06 13:51:52 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Environment } from '@react-three/drei'
+import { Model } from './Model'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Canvas
+        camera={{
+          position: [5, 5, 5],
+          fov: 50
+        }}
+        shadows
+      >
+        {/* Lighting setup */}
+        <ambientLight intensity={0.4} />
+        <directionalLight
+          position={[10, 10, 5]}
+          castShadow
+          intensity={1}
+          shadow-mapSize-width={2048}
+          shadow-mapSize-height={2048}
+        />
+        
+        {/* Environment for reflections */}
+        <Environment preset="apartment" />
+        
+        {/* Your 3D Model */}
+        <Model />
+        
+        {/* Camera controls */}
+        <OrbitControls
+          enablePan={true}
+          enableZoom={true}
+          enableRotate={true}
+          target={[0, 0, 0]}
+        />
+      </Canvas>
+    </div>
+  )
+}
+
+export default App
